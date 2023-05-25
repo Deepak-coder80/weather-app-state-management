13e1c0158adf27865956d41edc9326b04d29a6f1
Deepak M S <deepakcoder80@gmail.com>
2023-05-25 19:47:55 +0530

    skeltons

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:weather_cubit/pages/home_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -15,6 +16,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
+      home: const HomePage(),
     );
   }
 }
---
+++ lib/pages/search_page.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class SearchPage extends StatefulWidget {
+  const SearchPage({super.key});
+
+  @override
+  State<SearchPage> createState() => _SearchPageState();
+}
+
+class _SearchPageState extends State<SearchPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Search'),
+        elevation: 0,
+      ),
+      body: const Center(
+        child: Text('Seach'),
+      ),
+    );
+  }
+}
