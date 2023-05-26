d8f4facec8488b24d344f28feb10d143fcd5c791
Deepak M S <deepakcoder80@gmail.com>
2023-05-26 08:07:04 +0530

    search form

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,7 +1,9 @@
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http;
-import 'package:weather_cubit/repostories/weather_repository.dart';
-import 'package:weather_cubit/services/weather_services.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:weather_cubit/cubits/weather/weather_cubit.dart';
+import 'package:weather_cubit/pages/search_page.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -11,7 +13,16 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  
+  String? _city;
+  @override
+  void initState() {
+    _fetchWeather();
+    super.initState();
+  }
+
+  void _fetchWeather() {
+    context.read<WeatherCubit>().fetchWeather('london');
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -19,6 +30,20 @@ class _HomePageState extends State<HomePage> {
       appBar: AppBar(
         title: const Text('Weather'),
         elevation: 0,
+        actions: [
+          IconButton(
+            onPressed: () async {
+              _city = await Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => const SearchPage(),
+                ),
+              );
+              log('city: $_city');
+            },
+            icon: const Icon(Icons.search),
+          ),
+        ],
       ),
       body: const Center(
         child: Text('Home'),
--- lib/pages/search_page.dart
+++ lib/pages/search_page.dart
@@ -8,6 +8,21 @@ class SearchPage extends StatefulWidget {
 }
 
 class _SearchPageState extends State<SearchPage> {
+  final _formKey = GlobalKey<FormState>();
+  String? _city;
+  AutovalidateMode autovalidateMode = AutovalidateMode.disabled;
+
+  void _submit() {
+    setState(() {
+      autovalidateMode = AutovalidateMode.always;
+    });
+    final form = _formKey.currentState;
+    if (form != null && form.validate()) {
+      form.save();
+      Navigator.pop(context, _city!.trim());
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -15,8 +30,50 @@ class _SearchPageState extends State<SearchPage> {
         title: const Text('Search'),
         elevation: 0,
       ),
-      body: const Center(
-        child: Text('Seach'),
+      body: Form(
+        key: _formKey,
+        autovalidateMode: autovalidateMode,
+        child: Column(
+          children: [
+            const SizedBox(
+              height: 60.0,
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(
+                horizontal: 30,
+              ),
+              child: TextFormField(
+                autofocus: true,
+                style: const TextStyle(fontSize: 18),
+                decoration: const InputDecoration(
+                    labelText: 'City Name',
+                    hintText: 'More that 2 Characters',
+                    prefixIcon: Icon(Icons.search),
+                    border: OutlineInputBorder()),
+                validator: (String? input) {
+                  if (input == null || input.trim().length < 2) {
+                    return 'City name atleast 2 characters long';
+                  }
+                  return null;
+                },
+                onSaved: (String? input) {
+                  _city = input;
+                },
+              ),
+            ),
+            const SizedBox(
+              height: 20.0,
+            ),
+            ElevatedButton(
+                onPressed: _submit,
+                child: const Text(
+                  'How\'s the weather?',
+                  style: TextStyle(
+                    fontSize: 20.0,
+                  ),
+                ))
+          ],
+        ),
       ),
     );
   }
